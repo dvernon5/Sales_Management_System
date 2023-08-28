df6e9ef3ad83c48f497eab3d428c412c6e42b2cd
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-08-28 13:18:26 -0700

    Add the following header tags and libraries that will be needed

--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::vector;
+using std::ofstream;
+using std::ifstream;
 
