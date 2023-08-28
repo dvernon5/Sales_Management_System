9b34bee58322d39b4e03b4126ee39ee4c67d5bda
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-08-28 13:29:11 -0700

    Implemented a Game Class
    This class represents a video game in the store's inventory. It stores information about the title of the game, its price, and the quantity available in stock. It has methods to retrieve the title, price, and quantity of the game, as well as a method to update the quantity. This class is used to create and manage game objects within the store's inventory.

--- main.cpp
+++ main.cpp
@@ -11,3 +11,16 @@ using std::vector;
 using std::ofstream;
 using std::ifstream;
 
+class Game {
+ public:
+  Game(const string &title, int quantity, double game_price) : game_title(title), game_quantity(quantity), game_price(price) {}
+  void set_game(int new_quantity) { game_quantity = new_quantity; }
+  string get_title() const { return game_title; }
+  int get_quantity() const { return game_quantity; }
+  double get_price() const { return game_price; }
+
+ private: 
+  string game_title;
+  int game_quantity;
+  double game_price;
+};
